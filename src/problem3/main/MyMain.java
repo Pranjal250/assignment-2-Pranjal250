fdaa0e9378a5a1a149009abbd3ab44c30a5b335c
Abhishek chaudhary <abhishek.chaudhary_cs18@gla.ac.in>
2020-03-28 17:33:42 +0530

    Student and node class is created

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,21 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+
+import problem3.myqueue.MyPriorityQueue;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String args[]) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.insert("Pranjal",59);
+        obj.insert("Jain",19);
+        obj.insert("Soni",20);
+        obj.insert("Srasthi",2);
+        obj.insert("Mohit",53);
+        System.out.println("Students according to their roll number are: ");
+        obj.peep();
+    }
 }
