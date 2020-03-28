dc711a75bb397d6601b04c26a397bc395d661b45
Abhishek chaudhary <abhishek.chaudhary_cs18@gla.ac.in>
2020-03-28 16:47:53 +0530

    main class is created

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,22 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String args[]) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        obj.insert(10);
+        obj.insert(2);
+        obj.insert(5);
+        obj.insert(10);
+        obj.insert(11);
+        obj.insert(5);
+        obj.insert(69);
+        obj.insert(58);
+        System.out.print("Left Child nodes are: ");
+        obj.printLeftNode(obj.root);
+        System.out.println();
+        System.out.println("No of nodes that dosn't have a left child node are: "+ obj.getCount());
+    }
 }
